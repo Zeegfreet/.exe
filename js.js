ad03b1b354637f4173affd1885e8db985adf9e6d
Wesley Felipe Silva Arruda <lynx@MacBook-Air-de-Wesley.local>
2024-10-29 21:54:44 -0300

    Exe do Zim

--- js.js
+++ js.js
@@ -1,18 +1,49 @@
 const inputLeds = document.querySelector("input[name='ledsQuantity']")
+const inputPotency = document.querySelector("input[name='ledsPotency']")
+const inputFontVoltage = document.querySelector("input[name='fontTension']")
+
+const localKey = "@App:default"
 
 
-inputLeds.addEventListener('change', onChangeQuantiti)
+inputLeds.addEventListener('change', onChangeFields)
+inputPotency.addEventListener('change', onChangeFields)
+inputFontVoltage.addEventListener('change', onChangeFields)
+
+onLoadPage()
+
+function onLoadPage(){
+    const object = localStorage.getItem(localKey)
+    if (object) {
+        const values = JSON.parse(object)
+        inputLeds.value = values['quantity']
+        inputPotency.value = values['potency']
+        inputFontVoltage.value = values['fontVoltage']
+        changeComponents(values)
+    }
+};
+
+function onChangeFields(){
+    const quantity = Number(inputLeds.value)
+    const potency = Number(inputPotency.value)
+    const fontVoltage = Number(inputFontVoltage.value)
+
+    onRefreshCalcs(quantity, potency, fontVoltage)
+};
 
 
-function onChangeQuantiti(event){
-    const value = Number(event.target.value)
-    const resultMultiplicado = value * 1.7
-    const resultDividido = value / 2
-    const resultMultiplicado50 = value * 0.5
-    const resultMultiplicado30 = value * 0.3
+function onRefreshCalcs(quantity, potency, fontVoltage){
+    console.log(fontVoltage)
+    const totalPotency = quantity * potency
+    const resultDividido = totalPotency / fontVoltage
+    const resultMultiplicado50 = resultDividido * 1.5
+    const resultMultiplicado30 = resultDividido * 1.3
 
-    changeComponents({resultMultiplicado, resultDividido, resultMultiplicado50, resultMultiplicado30})
-}
+    changeComponents({quantity, potency, fontVoltage, totalPotency, resultDividido, resultMultiplicado50, resultMultiplicado30})
+};
+
+function onSaveState(state){
+    localStorage.setItem(localKey, JSON.stringify(state))
+};
 
 function changeComponents(values){
     Object.keys(values).forEach((key) => {
@@ -22,4 +53,5 @@ function changeComponents(values){
                 .toLocaleString('pt-BR', { minimumFractionDigits: 2 })
         } 
     })
-}
+    onSaveState(values)
+};
